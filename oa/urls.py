b6dab7960ad45ad05e52f4c24b201d117eccbbaf
KevinBrother <1301239018@qq.com>
2019-09-05 17:36:47 +0800

    投票

--- oa/urls.py
+++ oa/urls.py
@@ -16,7 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from vote import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hrs/', include('hrs.urls')),
+    path('', views.show_subjects),
+    path('teachers/', views.show_teachers),
+    path('praise/', views.praise_or_criticize),
+    path('criticize/', views.praise_or_criticize),
 ]
